e4a73576643976e5d2f0619754003cfbdc13ec04
Shraddha <dasgudeshhraddha@gmail.com>
2025-10-30 00:22:27 +0530

    fix: updated htaccess and dockerfile

--- project/config.php
+++ project/config.php
@@ -1,5 +1,9 @@
+<!-- <?php
+
+// $conn = mysqli_connect('localhost','','','shop_db') or die('connection failed');
+
+?> -->
 <?php
-
-$conn = mysqli_connect('localhost','','','shop_db') or die('connection failed');
-
+// Temporarily disable database connection for deployment testing
+$conn = false;  // add this line so $conn exists and won't cause fatal errors
 ?>
